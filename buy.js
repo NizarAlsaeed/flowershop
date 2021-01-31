673cdb236dfea697b99e8d533b79d7c5c27cf1e9
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-01-31 15:46:19 +0200

    adding css

--- buy.js
+++ buy.js
@@ -7,14 +7,10 @@ function addtoBasket(e){
 }
 
 function showrating(s){
-    console.log(s);
     var rate='<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F135706-409518-2-raikfcquaxqncofqfm.stackpathdns.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fyellow-star-150x150.png&f=1&nofb=1" />';
-    console.log(rate);
     for (var i=0; i<s;i++){
-        console.log('i='+i);
         document.getElementById("ratestr").innerHTML+=' '+rate;
     }
-    console.log('afteri='+i);
 }
 
 function buyNow(){
@@ -28,10 +24,8 @@ if(confirm(" Are you sure you want to checkout?")){
         }
         alert("Thank you for shopping from The Bycycle Shop");
         showrating(stars);
-        console.log(stars);
         }
     else {alert("Your Basket is empty");}
     }
-
 }
 }
